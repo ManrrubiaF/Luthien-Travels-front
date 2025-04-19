69bb3e6f2750d68fc7412cc23eadd2a4d49a1976
ManrrubiaF <manrrubiaf@gmail.com>
2025-04-19 09:15:24 -0300

    new navbar style

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -4,10 +4,9 @@ export default function NavBar() {
 
     return (
         <nav className={Styles.navMayor}>
-            <img className={Styles.patita1} src='/assets/images/patita.svg' alt='patita2' />
+            <img className={Styles.logo} src='/logo.jpg' alt='logo' />
             <h1> Luthien  Pet Travels
                 < br /> Traslado de mascotas</h1>
-            <img className={Styles.patita2} src='/assets/images/patita.svg' alt='patita1' />
         </nav>
     )
 }
