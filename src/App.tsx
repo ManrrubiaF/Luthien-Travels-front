c26cbcd033897e9577bf5567daf2a3babb0113e0
ManrrubiaF <manrrubiaf@gmail.com>
2025-04-27 10:56:57 -0300

    Seo Modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useEffect } from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import NavBar from './Components/NavBar/NavBar';
 
 function App() {
-  useEffect(()=>{
-    document.title = 'Luthien Travels'
-  },[])
+
   return (
     <div>
       <NavBar />
